e0012bfa8ec204564ccab4146f08152d6b6b3a6e
Dmitriy Yakovlev <1d3ddd@gmail.com>
2022-01-19 22:29:25 +0200

    feat:add solutions

--- app.js
+++ app.js
@@ -1,21 +1,55 @@
-function dataReverse(data) {
-  const bytes=data.length/8
-  let newArr=[]
-  let resultArr=[]
-  for (let j=1;j<=bytes;j++){
-  newArr=[]
-  for (let i=0;i<8;i++){
-    newArr.push(data[0])
-    data.shift()
-  }
-  resultArr.unshift(...newArr)
-}
- 
-  return resultArr
-  }
-  console.log(dataReverse([1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,1,0,1,0,1,0]));
-  console.log(dataReverse([0,0,1,1,0,1,1,0,0,0,1,0,1,0,0,1]));
 
-  document.writeln(dataReverse([1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,1,0,1,0,1,0]))
- 
+// solution 1
+// function killer(suspectInfo, dead) {
+//   return Object.keys(suspectInfo).find(x => dead.every(y => suspectInfo[x].includes(y)))
+//  }
+
+// solution 2
+// function killer(suspectInfo, dead) {
   
+//   for (let name in suspectInfo) {
+//     if (dead.every(deadPerson => suspectInfo[name].includes(deadPerson))) {
+//       return name;
+//     }
+//   }
+// }
+
+// solution 3
+// function killer(obj, arr) {
+//   for (const key in obj){
+//     let same = 0;
+//     for(let i = 0;i<arr.length;i++){
+//       if(obj[key].includes(arr[i])){
+//         same++
+//       }
+//        if(same === arr.length){
+//          return key
+//        }
+//     }
+//   }
+//   }
+
+// solution 4
+  // function killer(obj, dead) {
+  //   for (let i in obj) 
+  //     if (obj[i].filter(i => dead.includes(i)).length == dead.length) return i
+  // }
+
+// solution 5
+  // const killer = (suspectInfo, dead) =>
+  // Object.keys(suspectInfo).find(val => dead.every(v => suspectInfo[val].includes(v)));
+
+ // solution 6
+  function killer(suspectInfo, dead) {
+    return  Object.entries(suspectInfo).find(([key, value]) => value.filter(num => dead.includes(num)).length==dead.length )[0]
+  }
+
+  
+  console.log(killer({'James': ['Jacob', 'Bill', 'Lucas'],
+  'Johnny': ['David', 'Kyle', 'Lucas'],
+  'Peter': ['Lucy', 'Kyle']},['Lucas', 'Bill']));
+
+  document.writeln(killer({'James': ['Jacob', 'Bill', 'Lucas'],
+  'Johnny': ['David', 'Kyle', 'Lucas'],
+  'Peter': ['Lucy', 'Kyle']},['Lucas', 'Bill']))
+ 
